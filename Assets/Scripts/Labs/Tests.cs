c6897aefa7f9a1f02fe19530e8a29569a77e0d0d
ScSmlik <2738030477@qq.com>
2023-10-07 13:51:10 +0800

    VS测试2

--- Assets/Scripts/Labs/Tests.cs
+++ Assets/Scripts/Labs/Tests.cs
@@ -7,7 +7,7 @@ public class Tests : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("1");
+        Debug.Log("3");
     }
 
     // Update is called once per frame
